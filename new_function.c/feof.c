7506c08f4ebc64e3085cbd2f3f25e194e6e6977f
Aayus Regmi <aayusregmi721@gmail.com>
2026-04-30 23:58:01 +0545

    something crazy

--- new_function.c/feof.c
+++ new_function.c/feof.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-//it is used to detect the end of file
-int main(){
-    FILE  *fp; char c;
-    fp = fopen("randomly.txt" , "r");
+// it is used to detect the end of file
+int main()
+{
+    FILE *fp;
+    char c;
+    fp = fopen("randomly.txt", "r");
     if (fp == NULL)
     {
         printf("The file doesnot exist!!");
     }
-    else{
+
+    else
+    {
+        c = getc(fp);
         while (!feof(fp))
         {
+            printf("%c", c);
             c = getc(fp);
-            printf("%c" , c);
         }
         fclose(fp);
-        
     }
-    
+
     return 0;
 }
